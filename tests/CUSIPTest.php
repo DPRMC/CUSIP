62f52d7905f65bbf82e1c16572a1a5dfee7f44f6
Michael Drennen <michaeldrennen74@gmail.com>
2019-01-04 14:56:49 -0700

    Updated some tests.

--- tests/CUSIPTest.php
+++ tests/CUSIPTest.php
@@ -1,29 +1,45 @@
 <?php
+
 use PHPUnit\Framework\TestCase;
 use DPRMC\CUSIP;
 
 class CUSIPTest extends TestCase {
 
+    /**
+     * @test
+     */
     public function testEmptyInputString() {
         $isCusip = CUSIP::isCUSIP( '' );
         $this->assertFalse( $isCusip );
     }
 
+    /**
+     * @test
+     */
     public function testInvalidInputString() {
         $isCusip = CUSIP::isCUSIP( 'notValidCusip' );
         $this->assertFalse( $isCusip );
     }
 
+    /**
+     * @test
+     */
     public function testValidInputString() {
         $isCusip = CUSIP::isCUSIP( '222386AA2' );
         $this->assertTrue( $isCusip );
     }
 
+    /**
+     * @test
+     */
     public function testValidInputStringWithWhitespace() {
         $isCusip = CUSIP::isCUSIP( ' 222386AA2 ' );
         $this->assertTrue( $isCusip );
     }
 
+    /**
+     * @test
+     */
     public function testInputStringWithNewLines() {
         $string = "3137A96Y7
 3136A45X3
@@ -34,6 +50,9 @@ class CUSIPTest extends TestCase {
         $this->assertTrue( count( $validCusips ) == 4 );
     }
 
+    /**
+     * @test
+     */
     public function testInputStringWithNewLinesAndBlankLines() {
         $string = "
         3137A96Y7
@@ -49,6 +68,9 @@ class CUSIPTest extends TestCase {
         $this->assertTrue( count( $validCusips ) == 4 );
     }
 
+    /**
+     * @test
+     */
     public function testInputStringWithNewLinesAndBlankLinesAndCommas() {
         $string = "
         3137A96Y7,
@@ -65,7 +87,7 @@ class CUSIPTest extends TestCase {
     }
 
     /**
-     *
+     * @test
      */
     public function testInputStringWithNewLinesAndBlankLinesAndCommasAndDuplicates() {
         $string      = "
@@ -81,24 +103,36 @@ class CUSIPTest extends TestCase {
         $this->assertTrue( count( $validCusips ) == 2 );
     }
 
+    /**
+     * @test
+     */
     public function testIsCusipWithStar() {
         $cusip   = '00800*AA0';
         $isCusip = CUSIP::isCUSIP( $cusip );
         $this->assertTrue( $isCusip );
     }
 
+    /**
+     * @test
+     */
     public function testIsCusipWithAmpersand() {
         $cusip   = '00800@AA8';
         $isCusip = CUSIP::isCUSIP( $cusip );
         $this->assertTrue( $isCusip );
     }
 
+    /**
+     * @test
+     */
     public function testIsCusipWithHash() {
         $cusip   = '00800#AA6';
         $isCusip = CUSIP::isCUSIP( $cusip );
         $this->assertTrue( $isCusip );
     }
 
+    /**
+     * @test
+     */
     public function testIsCusipWithInvalidCheckDigit() {
         $cusip   = '31397JYY5';
         $isCusip = CUSIP::isCUSIP( $cusip );
@@ -106,11 +140,21 @@ class CUSIPTest extends TestCase {
     }
 
     /**
-     *
+     * @test
      */
     public function testGetValidCusipsFromStringWithPregSplitFailure() {
         $fileResourceInsteadOfString = fopen( "./LICENSE", "r" );
         $validCusips                 = CUSIP::getValidCusipsFromString( $fileResourceInsteadOfString );
         $this->assertEquals( 0, count( $validCusips ) );
+    }
+
+
+    /**
+     * @test
+     */
+    public function cusipOfInvalidLengthShouldReturnFalse() {
+        $cusip         = '12345678987654321'; // Definitely longer than a CUSIP should be.
+        $shouldBeFalse = CUSIP::getChecksumDigit( $cusip );
+        $this->assertFalse( $shouldBeFalse );
     }
 }
